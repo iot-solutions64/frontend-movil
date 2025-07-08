e569a2bb9160f2fc0870f6341d07142c93f2df0a
Salvador Salinas <145953379+salvadoorssalinas@users.noreply.github.com>
2025-07-07 23:24:11 -0500

    feat: update baseUrl and getCrops endpoint

--- hydrosmart/lib/core/app_constants.dart
+++ hydrosmart/lib/core/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String baseUrl = 'http://192.168.1.40:8080/api/v1';
+  static const String baseUrl = 'https://api-hydrosmart.up.railway.app/api/v1';
 
   static const String signUpEndpoint = '/authentication/sign-up';
   static const String signInEndpoint = '/authentication/sign-in';
